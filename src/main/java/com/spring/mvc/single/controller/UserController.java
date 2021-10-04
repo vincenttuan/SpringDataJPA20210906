58f85a3b0c4c57c013047ff2af8d8bdee5bee743
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-10-04 21:22:54 +0800

    Add files via upload

--- src/main/java/com/spring/mvc/single/controller/UserController.java
+++ src/main/java/com/spring/mvc/single/controller/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.github.javafaker.Faker;
@@ -124,5 +125,10 @@ public class UserController {
 		return page.getContent();
 	}
 	
+	@GetMapping("/test/name")
+	@ResponseBody
+	public List<User> getByName(@RequestParam("name") String name) {
+		return userRepository.getByName(name);
+	}
 
 }
--- src/main/java/com/spring/mvc/single/repository/UserRepository.java
+++ src/main/java/com/spring/mvc/single/repository/UserRepository.java
@@ -1,11 +1,19 @@
 package com.spring.mvc.single.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.spring.mvc.single.entity.User;
 
+/*
+ * 方法命名規則
+ * 1. 查詢方法以 find | read | get 開頭
+ * 2. 涉及條件查詢時，條件的屬性（首字母大寫）用條件關鍵字（例如：And、Or）連結
+ * */
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-	
+	// 根據 name 來取得 User
+	List<User> getByName(String name);
 }
