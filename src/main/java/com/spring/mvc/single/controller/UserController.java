2c93b988c3dbca252d9d5a9e7b239ffe85b199fe
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-10-04 19:56:58 +0800

    Add files via upload

--- src/main/java/com/spring/mvc/single/controller/UserController.java
+++ src/main/java/com/spring/mvc/single/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +19,7 @@ import com.spring.mvc.single.repository.UserRepository;
 public class UserController {
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	// 新增範例資料
 	@GetMapping("/test/create_sample_data")
 	@ResponseBody
@@ -26,7 +27,7 @@ public class UserController {
 		Random r = new Random();
 		Faker faker = new Faker();
 		int count = 150;
-		for(int i=0;i<count;i++) {
+		for (int i = 0; i < count; i++) {
 			User user = new User();
 			user.setName(faker.name().lastName());
 			user.setPassword(String.format("%04d", r.nextInt(10000)));
@@ -36,13 +37,23 @@ public class UserController {
 		}
 		return "Create sample data ok !";
 	}
-	
-	// 查詢範例資料
+
+	// 查詢範例資料 1
 	@GetMapping("/test/findall")
 	@ResponseBody
 	public List<User> testFindall() {
 		List<User> users = userRepository.findAll();
 		return users;
 	}
-	
+
+	// 查詢範例資料 2
+	@GetMapping("/test/findall_sort")
+	@ResponseBody
+	public List<User> testFindallSort() {
+		// ASC 自然排序 小->大, DESC 大->小
+		Sort sort = new Sort(Sort.Direction.ASC, "name");
+		List<User> users = userRepository.findAll(sort);
+		return users;
+	}
+
 }
